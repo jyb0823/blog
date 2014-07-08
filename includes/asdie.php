918da0772df2b218443267687178830370b513d3
ybin <0bin823@naver.com>
2014-07-08 12:01:06 +0900

    Create includes/ and Add footer.php, topNav.php, aside.php/ Update style.css

---
+++ includes/asdie.php
@@ -0,0 +1,29 @@
+<aside>
+       <div id="profile">
+           <p>Profile</p>            
+           <img src="images/profile.jpg" style="width:150px; height:100px" alt="my profile image">
+           <p>개발자를 꿈꾸는 <strong>ybin</strong>의 블로그입니다. 충북대학교 4학년에 재학중이며, 이 곳은 저의 프로젝트를 올리고 공유하는 공간입니다.</p>
+           <div id="socialMediaIcon">
+             <a href="http://facebook.com/jyb0823"><img src="images/facebook.png" style="width:30px; height: 30px" alt=m"facebook"></a>
+             <a href="http://github.com/jyb0823"><img src="images/github.png" style="width:30px; height: 30px" alt=m"github"></a>
+             <a href="mailto:jyb0823@gmail.com"><img src="images/email.png" style="width:30px; height: 30px" alt=m"gmail"></a>
+          </div>
+       </div>
+       
+       <nav>
+           <ul class="topMenu">
+               <li><a href="/">Home</a></li>
+               <li>
+                   <a href="#"><b>스마트교육 콘텐츠 교육</b></a>                 
+                   <ul class="subMenu">
+                       <li><a href="#" onclick="ready();">Web Programming</a></li>
+                       <li><a href="#" onclick="ready();">Java</a></li>
+                       <li><a href="#" onclick="ready();">MySQL</a></li>
+                       <li><a href="#" onclick="ready();">Android</a></li>
+                   </ul>                   
+               </li>
+               <li><a href="#" onclick="ready();">Study</a></li>
+               
+           </ul>
+       </nav>
+</aside>
---
+++ includes/topNav.php
@@ -0,0 +1,5 @@
+<nav id="topNav">
+ 	 <a href="/index.html">home</a>
+ 	 <a href="#" onclick="ready();">contact</a>
+ 	 <a href="#" onclick="ready();">guest</a>
+</nav>
