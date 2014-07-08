171990edd220e7a544c7ac5d53003c23b2f31828
ybin <0bin823@naver.com>
2014-07-08 20:30:10 +0900

    Modify index.html -> index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+   include("includes/layout.php")
+?>
+<div id="container">   
+ 	
+   <header>
+     <h1>반갑습니다</h1>
+   </header>      
+   
+   <?php
+   include("includes/asdie.php")
+   ?>
+
+ </div>
+    
+<?php
+  include("includes/footer.php")
+?>
